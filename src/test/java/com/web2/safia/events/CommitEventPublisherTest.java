6d2bb26ed36ec6fd1429381ccb96eb1381bd3bea
Pedro Rabis <rabis.pedro@gmail.com>
2025-10-31 10:34:17 -0300

    feat: adicionado testes unitarios de publicacao de evento

---
+++ src/test/java/com/web2/safia/events/CommitEventPublisherTest.java
@@ -0,0 +1,86 @@
+package com.web2.safia.events;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.doNothing;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.context.ApplicationEventPublisher;
+
+import com.web2.safia.models.CommitType;
+import com.web2.safia.models.Employee;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class CommitEventPublisherTest {
+	private CommitEventPublisher underTest;
+
+	@Mock
+	private ApplicationEventPublisher applicationEventPublisher;
+
+	@BeforeAll
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+		underTest = new CommitEventPublisher(applicationEventPublisher);
+	}
+
+	@Test
+	void givenInvalidCommitEvent_whenPublishCommitEvent_thenReturnVoid() {
+		assertDoesNotThrow(() -> underTest.publishCreateCommitEvent("Descrição inválida", null));
+		assertDoesNotThrow(() -> underTest.publishCreateCommitEvent(null, new Employee()));
+		assertDoesNotThrow(() -> underTest.publishCreateCommitEvent(null, null));
+	}
+
+	@Test
+	void givenValidCommitEvent_whenPublishCreateCommitEvent_thenReturnVoid() {
+		var creator = new Employee();
+
+		var validCommitEvent = new CommitEvent(this, "Commit válido", CommitType.CRIACAO, creator);
+		doNothing().when(applicationEventPublisher).publishEvent(validCommitEvent);
+
+		assertDoesNotThrow(() -> underTest.publishCreateCommitEvent("Commit válido", creator));
+	}
+
+	@Test
+	void givenValidCommitEvent_whenPublishActivateCommitEvent_thenReturnVoid() {
+		var creator = new Employee();
+
+		var validCommitEvent = new CommitEvent(this, "Commit válido", CommitType.ATIVACAO, creator);
+		doNothing().when(applicationEventPublisher).publishEvent(validCommitEvent);
+
+		assertDoesNotThrow(() -> underTest.publishActivateCommitEvent("Commit válido", creator));
+	}
+
+	@Test
+	void givenValidCommitEvent_whenPublishDeactivateCommitEvent_thenReturnVoid() {
+		var creator = new Employee();
+
+		var validCommitEvent = new CommitEvent(this, "Commit válido", CommitType.DESATIVACAO, creator);
+		doNothing().when(applicationEventPublisher).publishEvent(validCommitEvent);
+
+		assertDoesNotThrow(() -> underTest.publishDeactivateCommitEvent("Commit válido", creator));
+	}
+
+	@Test
+	void givenValidCommitEvent_whenPublishUpdateCommitEvent_thenReturnVoid() {
+		var creator = new Employee();
+
+		var validCommitEvent = new CommitEvent(this, "Commit válido", CommitType.ATUALIZACAO, creator);
+		doNothing().when(applicationEventPublisher).publishEvent(validCommitEvent);
+
+		assertDoesNotThrow(() -> underTest.publishUpdateCommitEvent("Commit válido", creator));
+	}
+
+	@Test
+	void givenValidCommitEvent_whenPublishRemoveCommitEvent_thenReturnVoid() {
+		var creator = new Employee();
+
+		var validCommitEvent = new CommitEvent(this, "Commit válido", CommitType.REMOCAO, creator);
+		doNothing().when(applicationEventPublisher).publishEvent(validCommitEvent);
+
+		assertDoesNotThrow(() -> underTest.publishRemoveCommitEvent("Commit válido", creator));
+	}
+}
