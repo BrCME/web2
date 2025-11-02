2202cad14cf4f9ffe7c7e4d7aa2dfa382e646deb
Pedro Rabis <rabis.pedro@gmail.com>
2025-11-02 13:01:44 -0300

    Audit (#11)
    * wip: adicionado publicacao de eventos
    
    * wip: adicionando eventos e auditoria correta
    
    * wip: adicionado sistema de criacao de usuario
    
    * feat: adicionado sistema de login e criacao de usuario
    
    * feat: adicionado testes unitarios de publicacao de evento
    
    * refactor: arrumado alguns code smells do sonnar
    
    * refactor: melhorado publicacao de eventos

--- src/main/java/com/web2/safia/events/CommitEventPublisher.java
+++ src/main/java/com/web2/safia/events/CommitEventPublisher.java
@@ -8,34 +8,33 @@ import com.web2.safia.models.Employee;
 
 @Component
 public class CommitEventPublisher {
-	private final ApplicationEventPublisher applicationEventPublisher;
+	private ApplicationEventPublisher applicationEventPublisher;
 
 	public CommitEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
 		this.applicationEventPublisher = applicationEventPublisher;
 	}
 
-	public void publishUpdateCommitEvent(final String description, final Employee creator) {
-		var commitEvent = new CommitEvent(this, description, CommitType.ATUALIZACAO, creator);
+	private void publishCommitEvent(CommitEvent commitEvent) {
 		applicationEventPublisher.publishEvent(commitEvent);
 	}
 
-	public void publishActivateCommitEvent(final String description, final Employee creator) {
-		var commitEvent = new CommitEvent(this, description, CommitType.ATIVACAO, creator);
-		applicationEventPublisher.publishEvent(commitEvent);
+	public void publishUpdateCommitEvent(String description, Employee creator) {
+		publishCommitEvent(new CommitEvent(this, description, CommitType.ATUALIZACAO, creator));
 	}
 
-	public void publishCreateCommitEvent(final String description, final Employee creator) {
-		var commitEvent = new CommitEvent(this, description, CommitType.CRIACAO, creator);
-		applicationEventPublisher.publishEvent(commitEvent);
+	public void publishActivateCommitEvent(String description, Employee creator) {
+		publishCommitEvent(new CommitEvent(this, description, CommitType.ATIVACAO, creator));
 	}
 
-	public void publishDeactivateCommitEvent(final String description, final Employee creator) {
-		var commitEvent = new CommitEvent(this, description, CommitType.DESATIVACAO, creator);
-		applicationEventPublisher.publishEvent(commitEvent);
+	public void publishCreateCommitEvent(String description, Employee creator) {
+		publishCommitEvent(new CommitEvent(this, description, CommitType.CRIACAO, creator));
 	}
 
-	public void publishRemoveCommitEvent(final String description, final Employee creator) {
-		var commitEvent = new CommitEvent(this, description, CommitType.REMOCAO, creator);
-		applicationEventPublisher.publishEvent(commitEvent);
+	public void publishDeactivateCommitEvent(String description, Employee creator) {
+		publishCommitEvent(new CommitEvent(this, description, CommitType.DESATIVACAO, creator));
+	}
+
+	public void publishRemoveCommitEvent(String description, Employee creator) {
+		publishCommitEvent(new CommitEvent(this, description, CommitType.REMOCAO, creator));
 	}
 }
